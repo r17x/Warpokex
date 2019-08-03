9e016b263e2b38aba32c4ee3e8ef1a252593d7e6
ri7nz <ri7nz.labs@gmail.com>
2019-08-04 03:17:46 +0700

    chore: add atomize for  UI design library

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test.todo('User can browse pokemon in infinite list - User can view detailed information of each pokemon')
+test.todo('User can view the image of each pokemon')
+test.todo('User can filter list of pokemon based on an attribute of pokemon (feel free to choose one attribute to use for filtering i.e. nature, types)')
